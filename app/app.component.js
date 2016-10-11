f9ff1fd1db9d91bf5da8c50859cc16470f630bba
stefan-jj <stefanps2@hotmail.com>
2016-10-11 15:35:11 +0100

    External template files

--- app/app.component.js
+++ app/app.component.js
@@ -38,8 +38,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>Click a person to see their fact...</h2>\n    <ul class=\"people\">\n      <li *ngFor=\"let person of people\"\n        [class.selected]=\"person === selectedPerson\"\n        (click)=\"onSelect(person)\">\n        <span class=\"badge\">{{person.id}}</span> {{person.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedPerson\">\n      <h2>{{selectedPerson.name}}'s details!</h2>\n      <div><label><b>ID:</b> </label>{{selectedPerson.id}}</div>\n      <div>\n        <label><b>Name:</b> </label>\n        {{selectedPerson.name}}\n      </div>\n      <div>\n        <label><b>Fact:</b> </label>\n        {{selectedPerson.fact}}\n      </div>\n    </div>\n  ",
-            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .people {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .people li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .people li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .people li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .people .text {\n      position: relative;\n      top: -3px;\n    }\n    .people .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
+            templateUrl: 'app/template.html',
+            styleUrls: ['app/style.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
